2da4a0b5e5008ffd7d3afc7fd4a383ccb5acfb28
tapaighosh <tapaighosh2000@gmail.com>
2022-01-27 11:10:51 +0530

    create

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from re import A
-from flask import Flask
+from flask import Flask ,request
 import json
 import os
 from web3 import Web3
@@ -18,8 +18,8 @@ key='0x05ba5a15a4ac68580fe2a9c6980d869aa47c6c983f1b0f21b14713477bbe6970'
 account = web3.eth.account.privateKeyToAccount(key)
 # print(account.address)
 
-# path="C:/Users/MSI 1/Desktop/BlockChain/nft/nftBack/brawnie"
-# os.chdir(path)
+path="C:/Users/MSI 1/Desktop/BlockChain/nft/nftBack/brawnie"
+os.chdir(path)
 
 with open(f'{os.getcwd()}/build/contracts/NFT.json') as f:
         NFTdata = json.load(f)
@@ -53,15 +53,17 @@ def createAcccount():
     account= web3.eth.account.create()
     print(account)
     return {
-        "account address ": account,
+        "account_address": account,
         
     }
 
 @app.route('/import_wallet/<private_key>')
 def importAcccount(private_key):
     account = web3.eth.account.privateKeyToAccount(private_key)
+    balance=web3.eth.getBalance(account.address)
     return {
-        "account address ": account.address,
+        "account_address ": account.address,
+        "balance":balance
     }
 
 @app.route('/fetch')
@@ -82,9 +84,11 @@ def fatch():
         "details":details
     }
 
-@app.route('/create')
+@app.route('/create',methods=['POST'])
 def create():
-    transaction = NFTcontract.functions.createToken("https://ipfs.infura.io/ipfs/QmW6WpWf8TSvfAB3SpD8UuwKssdQMniA5sfEgRxZSTfLSs").buildTransaction({
+    data=request.values.get('data')
+    price=request.values.get('data')
+    transaction = NFTcontract.functions.createToken(data).buildTransaction({
     'gas': 2000000,
     'gasPrice': web3.toWei('50', 'gwei'),
     'from': account.address,
@@ -93,23 +97,27 @@ def create():
     signed_txn = web3.eth.account.signTransaction(transaction, private_key=key)
     result=web3.eth.sendRawTransaction(signed_txn.rawTransaction)
 
-    # tx_receipt = web3.eth.waitForTransactionReceipt(web3.toHex(result))
+    web3.eth.waitForTransactionReceipt(web3.toHex(result))
 
     listing = NFTmarket_contract.functions.getListingPrice().call()
 
-    # transaction = NFTmarket_contract.functions.createMarketItem(NFTaddress,web3.toHex(result),4000000,{"value": listing}).buildTransaction({
-    # 'gas': 2000000,
-    # 'gasPrice': web3.toWei('50', 'gwei'),
-    # 'from': account.address,
-    # 'nonce': web3.eth.get_transaction_count(account.address)
-    # }) 
-    # signed_txn = web3.eth.account.signTransaction(transaction, private_key=key)
-    # result=web3.eth.sendRawTransaction(signed_txn.rawTransaction)
-    # tx_receipt = web3.eth.waitForTransactionReceipt(web3.toHex(result))
+    data =web3.eth.getTransactionReceipt(web3.toHex(result))
+    logs = data.logs
+    transaction = NFTmarket_contract.functions.createMarketItem(NFTaddress,int(web3.toHex(logs[0]["topics"][3]),base=16),web3.toWei(price,'ether')).buildTransaction({
+    'gas': 2000000,
+    'gasPrice': web3.toWei('50', 'gwei'),
+    'from': account.address,
+    'nonce': web3.eth.get_transaction_count(account.address),
+    'value':int(listing)
+    }) 
+    signed_txn = web3.eth.account.signTransaction(transaction, private_key=key)
+    result=web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    res=web3.eth.waitForTransactionReceipt(web3.toHex(result))
 
     return {
-        "receipt":web3.toHex(result),
-        "type":str(listing) ,
+        "receipt":str(res),
+        "type":web3.toHex(result) ,
+        "logs":int(web3.toHex(logs[0]["topics"][3]),base=16)
     }
 
 if __name__=="__main__":
